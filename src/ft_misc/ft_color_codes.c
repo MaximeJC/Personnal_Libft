454d0bdfb497baa9555134a2d34287e54d757e81
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-03-06 15:08:28 +0100

    Add bold, italic, strike, underline and background color examples

--- src/ft_misc/ft_color_codes.c
+++ src/ft_misc/ft_color_codes.c
@@ -6,7 +6,7 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 14:04:14 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/03/06 14:16:25 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/03/06 14:49:22 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,13 @@ void	ft_color_codes(void)
 	ft_putendl_fd("\033[0;35mPurple \\033[0;35m", 1);
 	ft_putendl_fd("\033[0;36mCyan   \\033[0;36m", 1);
 	ft_putendl_fd("\033[0;37mWhite  \\033[0;37m", 1);
+	ft_putendl_fd("\033[1;37mBold White        \\033[1;37m]\033[0;0m", 1);
+	ft_putendl_fd("\033[1;31mBold Red          \\033[1;31m]\033[0;0m", 1);
+	ft_putendl_fd("\033[3;37mItalic White      \\033[3;37m]\033[0;0m", 1);
+	ft_putendl_fd("\033[4;37mUnderline White   \\033[2;37m]\033[0;0m", 1);
+	ft_putendl_fd("\033[9;37mStrike White      \\033[9;37m]\033[0;0m", 1);
+	ft_putendl_fd("\033[37;46mWhite on Cyan     \\033[37;46m]\033[0;0m", 1);
+	ft_putendl_fd("\033[1;31;46mBold Italic Red on White  \
+\\033[1;31;46m]\033[0;0m", 1);
 	ft_putendl_fd("\033[0;0mReset  \\033[0;0m", 1);
 }
