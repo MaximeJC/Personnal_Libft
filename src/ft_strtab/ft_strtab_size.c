ea6d75b00222492ec7cd5006517c32c7cca2f5a4
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-03-08 13:06:05 +0100

    Norm correction

--- src/ft_strtab/ft_strtab_size.c
+++ src/ft_strtab/ft_strtab_size.c
@@ -6,13 +6,13 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:05:21 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/30 15:07:10 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/03/08 10:38:05 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strtab_size(char	**strtab)
+int	ft_strtab_size(char **strtab)
 {
 	int	size;
 
