c81005f88a2710a95168ad28cab3e0d6c74f9cc8
Maxime Gouraud <mgouraud@student.42lehavre.fr>
2025-04-23 15:50:26 +0200

    Bugfix - Add security if src is NULL before dest calloc to avoid lost data leak

--- src/ft_str/ft_strdup.c
+++ src/ft_str/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:01:22 by mgouraud          #+#    #+#             */
-/*   Updated: 2024/12/30 16:33:13 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/04/23 15:40:59 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	*ft_strdup(const char *src)
 
 	src_len = ft_strlen(src);
 	i = 0;
+	if (src == NULL)
+		return (NULL);
 	dest = ft_calloc(src_len + 1, sizeof(char));
 	if (dest == NULL)
 		return (NULL);
